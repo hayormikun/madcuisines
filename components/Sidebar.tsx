08c78e0c272c12e20e5525f4619fb5f536248999
Favour Akomolafe <akomolafefavour@gmail.comm>
2022-07-03 09:13:05 +0100

    update pages

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ export interface SidebarProps {
 
 export const Sidebar = ({view, create, viewLink, createLink}: SidebarProps) => {
   return (
-    <aside className="h-auto lg:h-[90vh] bg-teal-700 shadow-md w-full lg:w-2/12">
+    <aside className="h-auto lg:min-h-[90vh] bg-teal-700 shadow-md w-full lg:w-2/12">
         <ul className="flex justify-center mt-10 pt-5 lg:block lg:text-center  lg:p-5">
           <Link href="/">
           <a className="hidden md:flex mb-5 text-gray-100 font-bold text-md p-4 lg:border-b-2 border-gray-200">Dashboard <HomeIcon className="ml-2 w-5"/></a>
---
+++ pages/category/update/[id].tsx
@@ -0,0 +1,50 @@
+import { FormEvent, useState } from "react"
+import { WideButton } from "../../../components/Button"
+import { Heading } from "../../../components/Heading"
+import { Sidebar } from "../../../components/Sidebar"
+import { ICategory } from "../../../libs/interfaces/ICategory"
+
+
+const update = () => {
+  const [name, setName] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+
+    const category: ICategory = {
+      name,
+      description
+    }
+
+    console.log(category)
+  }
+
+  return (
+    <main className='lg:flex pt-20'>
+      <Sidebar view='Categories' create='Category' viewLink='/category' createLink='/category/create' />
+        <div className="mt-5 w-full lg:w-10/12">
+          <Heading heading='Update Category' />
+
+          <div className="grid">
+              <form onSubmit={handleSubmit} action="POST" className="text-gray-700 font-semibold mx-auto w-6/12">
+                  <div className="grid gap-3 w-full my-5">
+                    <label htmlFor="name">Category Name:</label>
+                    <input className="p-2 w-full rounded border-2" type={"text"} id="name" placeholder="Category Name" value={name} onChange={(e)=> {setName(e.target.value)}} required/>
+                  </div>
+
+                  <div className="grid gap-3 w-full my-5">
+                    <label htmlFor="description">Category Description:</label>
+                    <textarea className="p-3  w-full rounded border-2" id="description" placeholder="Product description" value={description} onChange={(e)=>{setDescription(e.target.value)}} required></textarea>
+                  </div>
+              
+                <WideButton name="Update Category" />
+
+              </form>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default update
