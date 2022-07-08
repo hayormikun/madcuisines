72d9630a8cb1717d8a7b983e41b06e04ba384049
Favour Akomolafe <akomolafefavour@gmail.comm>
2022-07-08 09:17:45 +0100

    create orders comment

--- libs/interfaces/IOrders.ts
+++ libs/interfaces/IOrders.ts
@@ -1,14 +1,3 @@
-// export type Priority =  'high' | 'normal' | 'low'
-
-export interface IOrders {
-    deliveryId: string
-    orderId: string
-    dispatcher: string
-    priority: string
-    receiver: string
-    receiverPhone: string
-    note: string
-}
 
 export interface IOrder {
     userId: string
